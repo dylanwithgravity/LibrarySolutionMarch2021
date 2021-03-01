428cceb4bc2151e10c00114a8aaa3ef77c2f168f
Dylan Williamson <dylanwithgravity@gmail.com>
2021-03-01 15:19:18 -0500

    before monday break

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -28,11 +28,35 @@ namespace LibraryApi.Controllers
             return Ok($"Getting info about customer: {customerId}");
         }
 
-        [HttpGet("blogs/{year:int}/{month:int}/{day:int}")]
+        [HttpGet("blogs/{year:int}/{month:int:min(1):max(12)}/{day:int}")]
         public ActionResult GetBlogPosts(int year, int month, int day)
         {
             return Ok($"Getting blogs for {month} - {day} - {year}");
         }
+
+        // GET /employees
+        [HttpGet("employees")]
+        public ActionResult GetEmployees([FromQuery] string department = "All")
+        {
+            var response = new GetEmployeeReponse
+            {
+                Data = new List<string> { "Joe", "Sue", "Mary" },
+                Department = department
+            };
+            return Ok(response);
+        }
+
+        [HttpGet("whoami")]
+        public ActionResult GetWhoAmI([FromHeader(Name = "User-Agent")]string userAgent)
+        {
+            return Ok($"I have no idea, but you are running {userAgent}");
+        }
+    }
+
+    public class GetEmployeeReponse
+    {
+        public List<string> Data { get; set; }
+        public string Department { get; set; }
     }
 
     public class StatusResponse
